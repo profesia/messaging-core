4fac1df5d91eff75fd1528f006600cb3c6558344
Matej Bádal <badal@profesia.sk>
2022-09-21 21:41:04 +0200

    Added tests

---
+++ tests/Unit/Broking/Decorator/FailedMessagesPersisterTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Profesia\MessagingCore\Unit\Broking\Decorator;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\MockInterface;
+use Profesia\MessagingCore\Broking\Decorator\FailedMessagesPersister;
+use Profesia\MessagingCore\Broking\Dto\BrokingBatchResponse;
+use Profesia\MessagingCore\Broking\Dto\BrokingStatus;
+use Profesia\MessagingCore\Broking\Dto\DispatchedMessage;
+use Profesia\MessagingCore\Broking\Dto\MessageCollection;
+use Profesia\MessagingCore\Broking\MessageBrokerInterface;
+use Profesia\MessagingCore\Persistence\DispatchedEventRepositoryInterface;
+use Profesia\MessagingCore\Test\Unit\Helper;
+use Mockery;
+
+class FailedMessagesPersisterTest extends MockeryTestCase
+{
+    use Helper;
+
+    public function testCanPublish(): void
+    {
+        $messages         = static::createMessages(5);
+        $collection       = MessageCollection::createFromMessagesAndChannel(
+               'channel',
+            ...$messages
+        );
+
+        $index = 1;
+        $dispatchedMessages = [];
+        foreach ($messages as $message) {
+            $dispatchedMessages[] = new DispatchedMessage(
+                $message,
+                new BrokingStatus(
+                    $index % 2 !== 0,
+                    $index % 2 !== 0 ? null: "Reason{$index}"
+                )
+            );
+
+            $index++;
+        }
+
+        $expectedResponse = BrokingBatchResponse::createForMessagesWithIndividualStatus(...$dispatchedMessages);
+
+
+        /** @var MessageBrokerInterface|MockInterface $broker */
+        $broker = Mockery::mock(MessageBrokerInterface::class);
+        $broker
+            ->shouldReceive('publish')
+            ->once()
+            ->withArgs(
+                [
+                    $collection,
+                ]
+            )->andReturn(
+                $expectedResponse
+            );
+
+        $failedMessages = array_filter(
+            $expectedResponse->getDispatchedMessages(),
+            function (DispatchedMessage $dispatchedMessage): bool
+            {
+                return ($dispatchedMessage->wasDispatchedSuccessfully() === false);
+            }
+        );
+
+        /** @var DispatchedEventRepositoryInterface|MockInterface $repository */
+        $repository = Mockery::mock(DispatchedEventRepositoryInterface::class);
+        $repository
+            ->shouldReceive('persistBatch')
+            ->once()
+            ->withArgs(
+                [
+                    ...$failedMessages,
+                ]
+            );
+
+        $decorator = new FailedMessagesPersister(
+            $broker,
+            $repository
+        );
+
+        $actualResponse = $decorator->publish($collection);
+        $this->assertEquals($expectedResponse, $actualResponse);
+    }
+}
