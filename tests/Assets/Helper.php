eef7faf4ad5395a5e72dbdf0528e21d8cb6b4f13
Tomas Sofranko <sofranko@profesia.sk>
2024-03-06 22:53:02 +0100

    Add AwsMessageBroker test

--- tests/Assets/Helper.php
+++ tests/Assets/Helper.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Profesia\MessagingCore\Test\Assets;
 
 use DateTimeImmutable;
+use Profesia\MessagingCore\Broking\Dto\Sending\AwsMessage;
 use Profesia\MessagingCore\Broking\Dto\Sending\Message;
 
 trait Helper
@@ -31,6 +32,33 @@ trait Helper
                 $forcedValues['data'] ?? [
                 'data' => $index,
             ]
+            );
+            $index++;
+        }
+
+        return $messages;
+    }
+
+    /**
+     * @param int $number
+     *
+     * @return AwsMessage[]
+     */
+    private static function createAwsMessages(int $number, array $forcedValues = [], int $startIndex = 1): array
+    {
+        $messages = [];
+        $index    = $startIndex;
+        for ($i = 1; $i <= $number; $i++) {
+            $messages[] = new AwsMessage(
+                $forcedValues['topic'] ?? "topic{$index}",
+                $forcedValues['provider'] ?? "provider{$index}",
+                $forcedValues['eventType'] ?? "eventType{$index}",
+                new DateTimeImmutable(),
+                $forcedValues['correlationId'] ?? 'correlationId',
+                $forcedValues['data'] ?? ['data' => $index],
+                $forcedValues['resource'] ?? "resource{$index}",
+                $forcedValues['objectId'] ?? "objectId{$index}",
+                $forcedValues['subscribeName'] ?? "subscribeName{$index}",
             );
             $index++;
         }
